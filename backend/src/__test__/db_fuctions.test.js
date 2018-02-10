2a3308d7273a6ae7fba6d4ecccfdd209dffa118b
Ethien Salinas <ethien.salinas@gmail.com>
2018-02-10 10:57:51 -0600

    [ADD] import notation

--- backend/src/__test__/db_fuctions.test.js
+++ backend/src/__test__/db_fuctions.test.js
@@ -1,16 +1,18 @@
-const db_functions = require('../db_functions')
+import db_functions  from '../db_functions'
+
+
+const obj2012 = {
+  "title": "Argo",
+  "imdbId": "tt1024648",
+  "releaseDate": "2012-10-04T04:00:00.000Z",
+  "releaseCountry": "USA",
+  "releaseYear": 2012,
+  "releaseMonth": 9,
+  "releaseDay": 4
+}
 
 describe('get values suite', () => {
   it('should read an element', () => {
-    const obj = {
-      "title": "Argo",
-      "imdbId": "tt1024648",
-      "releaseDate": "2012-10-04T04:00:00.000Z",
-      "releaseCountry": "USA",
-      "releaseYear": 2012,
-      "releaseMonth": 9,
-      "releaseDay": 4
-    }
-    expect(db_functions.readAnElementById("2012")).toEqual(obj);
+    expect(db_functions.readAnElementById("2012")).toEqual(obj2012);
   })
 })
