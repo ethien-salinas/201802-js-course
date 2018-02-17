935a8033a7d38a64782b87e9070effbe326e5707
Ethien Salinas <ethien.salinas@gmail.com>
2018-02-17 10:56:51 -0600

    [ADD] route to get a movie

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const app = express()
-const db = require('../db/movies.json')
 const dbFunctions = require('./db_functions')
 const PORT = 4000
 
-app.get('/', function (req, res) {
+
+app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/movies', function (req, res) {
+app.get('/movies', (req, res) => {
   res.send(dbFunctions.getAll())
+})
+
+app.get('/movies/:movieId', (req, res) => {
+  res.send(dbFunctions.getElementById(req.params.movieId))
 })
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
