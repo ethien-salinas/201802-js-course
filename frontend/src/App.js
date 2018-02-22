1143b5149154747939c7021bbcec9dbaeb979329
Ethien Salinas <ethien.salinas@gmail.com>
2018-02-22 07:35:33 -0600

    [ADD] sample remote call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,37 @@ import './App.css';
 import Header from './component/Header'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: {}
+    }
+  }
+
+  componentWillMount() {
+    // here we can start a loading animation
+    console.log('componentWillMount')
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:4000/movies')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ movies: data })
+      })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Edited!!!</p>
+        <p>{JSON.stringify(this.state.movies)}</p>
       </div>
     );
   }
