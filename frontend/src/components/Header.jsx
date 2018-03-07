ad48119367a1637492082f47dbdb52aab63f58ff
Ethien Salinas <ethien.salinas@gmail.com>
2018-03-07 07:28:12 -0600

    [ADD] hero style to Header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,10 +4,17 @@ import logo from '../logo.svg';
 class Header extends Component {
   render() {
     return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
+      <section className="hero is-dark">
+        <div className="hero-body">
+          <div className="container">
+            <figure class="image is-128x128">
+              <img src={logo} className="App-logo" alt="logo" />
+            </figure>
+            <h1 className="title">React App</h1>
+            <h2 className="subtitle">with bulma</h2>
+          </div>
+        </div>
+      </section>
     )
   }
 }
